a1c043a8cf39d8637f4273bd1d1703a1f6512209
philchute <philchute@gmail.com>
2025-06-22 20:30:40 -0400

    static files fix

--- Program.cs
+++ Program.cs
@@ -23,8 +23,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
-app.MapStaticAssets();
-app.MapRazorPages().WithStaticAssets();
+app.MapRazorPages();
 app.Run();
